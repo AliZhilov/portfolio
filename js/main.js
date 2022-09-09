30ca148633f6f08f2d7371d9f5d157241c80f744
AliZhilov <110725562+AliZhilov@users.noreply.github.com>
2022-09-09 15:40:46 +0300

    3d designer portfolio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('.slider').slick({
+    dots: true,
+    
+    nextArrow: '<button class="slick-arrow slick-next"><img class="slider__btn" src="images/next__arrow.svg" alt="next__arrow"></button>',
+    prevArrow: '<button class="slick-arrow slick-prev"><img class="slider__btn" src="images/prev__arrow.svg" alt="prev__arrow"></button>'
+  });
+});
